9a5ac100daef7a39b07954b397a6074bc6e9c82e
Pattra Yuktanon <pattra.th@gmail.com>
2019-04-15 22:52:17 -0700

    Update README.md

--- ui/src/index.js
+++ ui/src/index.js
@@ -5,7 +5,6 @@ import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
-import gql from "graphql-tag";
 
 const client = new ApolloClient({
   uri: process.env.REACT_APP_GRAPHQL_URI
@@ -16,19 +15,6 @@ const Main = () => (
     <App />
   </ApolloProvider>
 );
-
-client
-  .query({
-    query: gql`
-      {
-        Movie {
-          movieId
-          title
-        }
-      }
-    `
-  })
-  .then(response => console.log(response.data.title));
 
 ReactDOM.render(<Main />, document.getElementById("root"));
 registerServiceWorker();
