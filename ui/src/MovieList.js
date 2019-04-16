dc071c59036d2f343c550eec4b53e8bebc323717
Pattra Yuktanon <pattra.th@gmail.com>
2019-04-15 21:21:42 -0700

    Update movie's genres schema

--- ui/src/MovieList.js
+++ ui/src/MovieList.js
@@ -1,48 +1,18 @@
 import React, { Component } from "react";
 import { Item } from "semantic-ui-react";
-import { Query } from "react-apollo";
-import gql from "graphql-tag";
-
-const FEED_QUERY = gql`
-  {
-    Movie(title: $title) {
-      movieId
-      title
-      year
-      imdbRating
-      plot
-      poster
-      recommend(offset: 3) {
-        title
-        poster
-      }
-      sameGenre(offset: 3) {
-        title
-        poster
-      }
-    }
-  }
-`;
-
-const queryMovie = title => (
-  <Query query={FEED_QUERY} variable={{ title }}>
-    {({ loading, error, data }) => {
-      if (loading) return <div>Loading...</div>;
-      if (error) return <div>Error has occured!</div>;
-      if (data.movie.length === 0) return <div>No movie with this title!</div>;
-    }}
-  </Query>
-);
+// import { Query } from "react-apollo";
+// import gql from "graphql-tag";
+// import { threadId } from "worker_threads";
 
 class MovieList extends Component {
   render() {
-    const movie = queryMovie;
+    // const movie = queryMovie;
 
     return (
       <Item>
-        <Item.Image size="large" src={movie.poster} />
+        <Item.Image size="large" src={this.props.poster} />
         <Item.Content>
-          <Item.Header as="a">{movie.title}</Item.Header>
+          <Item.Header as="a">{this.props.title}</Item.Header>
           <Item.Meta>Description</Item.Meta>
           <Item.Extra>Additional Details</Item.Extra>
         </Item.Content>
@@ -50,5 +20,26 @@ class MovieList extends Component {
     );
   }
 }
+
+// const MOVIE_LIST_DATA = gql`
+//   {
+//     Movie(title: $title) {
+//       movieId
+//       title
+//       year
+//       imdbRating
+//       plot
+//       poster
+//       recommend(offset: 3) {
+//         title
+//         poster
+//       }
+//       sameGenre(offset: 3) {
+//         title
+//         poster
+//       }
+//     }
+//   }
+// `;
 
 export default MovieList;
